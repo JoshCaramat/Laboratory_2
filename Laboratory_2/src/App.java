482e22da0bd74d4d42c758b58adec7864df0c189
Josh <115612491+JoshCaramat@users.noreply.github.com>
2022-11-19 16:05:33 +0800

    Add files via upload

---
+++ Laboratory_2/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        cylinder cl = new cylinder();
+        cl.startUp();
+    }
+}
---
+++ Laboratory_2/src/cylinder.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+import java.text.DecimalFormat;
+public class cylinder{
+    private final DecimalFormat df = new DecimalFormat("0.00");
+    private double radius, height;
+    private double cylinderVolume, surfaceArea;
+    private Scanner sc = new Scanner(System.in);
+
+    private void setCylinderVolume(double radius, double height){
+        cylinderVolume = Math.PI * Math.pow(radius, 2) * height;
+    }
+    private double getVolume(){
+        return cylinderVolume;
+    }
+    private void setSurfaceArea(double radius, double height){
+        surfaceArea = 2 * Math.PI * radius * height + 2 * Math.PI * Math.pow(radius, 2);
+    }
+    private double getSurfaceArea(){
+        return surfaceArea;
+    }
+    private void userInputRadius(){
+        System.out.print("Enter Radius: ");
+        radius = sc.nextDouble();
+    }
+    private void userInputHeight(){
+        System.out.print("Enter Height: ");
+        height = sc.nextDouble();
+    }
+    private void equationOption(){
+        System.out.println("\nPick equation: \n1.) Volume of Cylinder\n2.) Surface Area of Cylinder");
+        int chosen = sc.nextInt();
+        switch(chosen){
+            case 1 -> {
+                setCylinderVolume(radius, height); 
+                System.out.println("\nV = " + df.format(getVolume()));
+            }
+            case 2 -> {
+                setSurfaceArea(radius, height); 
+                System.out.println("\nA = " + df.format(getSurfaceArea()));
+            }
+        }
+    }
+    public void startUp(){
+        userInputRadius();
+        userInputHeight();
+        equationOption();
+    }
+}
